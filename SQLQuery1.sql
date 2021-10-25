edf2461947a64624e0cdd0a9ebf92148c9ad4d26
Amit <amitkr24031995@gmail.com>
2021-10-25 23:09:04 +0530

    Inserted Data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -9,3 +9,8 @@ employee_salary int,
 employee_Start_date varchar(50))
 
 select *from employee_payroll;
+
+insert into employee_payroll values(1,'Amit',18000,'15Sep');
+insert into employee_payroll values(2,'Krishna',57000,'17Sep');
+insert into employee_payroll values(3,'Satty',11000,'11Oct');
+insert into employee_payroll values(4,'Lala',23000,'1Aug');
