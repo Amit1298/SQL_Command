23365367a2f9427fa51e5c16786a7e6875e18d65
Amit <amitkr24031995@gmail.com>
2021-10-25 22:59:49 +0530

    Adding table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,11 @@
 create database payroll_services
 
 use payroll_services
+
+create table employee_payroll(
+employee_id int,
+employee_name varchar(50),
+employee_salary int,
+employee_Start_date varchar(50))
+
+select *from employee_payroll;
